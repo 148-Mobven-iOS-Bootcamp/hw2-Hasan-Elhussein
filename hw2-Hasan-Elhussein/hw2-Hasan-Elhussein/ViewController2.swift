72d77c70b2f02d1d0687fbe17e37445f8ba54b6d
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2021-12-24 22:04:03 +0300

    Add ViewController2

---
+++ hw2-Hasan-Elhussein/hw2-Hasan-Elhussein/ViewController2.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController2.swift
+//  hw2-Hasan-Elhussein
+//
+//  Created by Hasan Elhussein on 24.12.2021.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
