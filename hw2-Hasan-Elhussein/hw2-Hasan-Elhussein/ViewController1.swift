7ecb4112d9b5b6e6a9dee977a8366de130311845
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2021-12-24 23:39:03 +0300

    Add IBAction codes for the UIButtons

--- hw2-Hasan-Elhussein/hw2-Hasan-Elhussein/ViewController1.swift
+++ hw2-Hasan-Elhussein/hw2-Hasan-Elhussein/ViewController1.swift
@@ -8,26 +8,52 @@
 import UIKit
 
 class ViewController1: UIViewController {
-
+    
+    @IBOutlet weak var num1Txt: UITextField!
+    @IBOutlet weak var num2Txt: UITextField!
+    @IBOutlet weak var resultTxt: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     
+    @IBAction func addBtnTapped(_ sender: Any) {
+        if let num1 = Int(num1Txt.text!), let num2 = Int(num2Txt.text!){
+            resultTxt.text = String(calculate(num1, operand: add, num2))
+        }
+    }
+    @IBAction func subBtnTapped(_ sender: Any) {
+        if let num1 = Int(num1Txt.text!), let num2 = Int(num2Txt.text!){
+            resultTxt.text = String(calculate(num1, operand: subtract, num2))
+        }
+    }
+    @IBAction func mulBtnTapped(_ sender: Any) {
+        if let num1 = Int(num1Txt.text!), let num2 = Int(num2Txt.text!){
+            resultTxt.text = String(calculate(num1, operand: multiply, num2))
+        }
+    }
+    @IBAction func divBtnTapped(_ sender: Any) {
+        if let num1 = Int(num1Txt.text!), let num2 = Int(num2Txt.text!){
+            resultTxt.text = String(calculate(num1, operand: divide, num2))
+        }
+    }
+    
 }
 
 // Math operations (+ - * /)
-func sum(_ num1: Int, _ num2: Int) -> Int{
+func add(_ num1: Int, _ num2: Int) -> Int{
     return num1 + num2
-}
-
-func multiply(_ num1: Int, _ num2: Int) -> Int{
-    return num1 * num2
 }
 
 func subtract(_ num1: Int, _ num2: Int) -> Int{
     return num1 - num2
+}
+
+func multiply(_ num1: Int, _ num2: Int) -> Int{
+    return num1 * num2
 }
 
 func divide(_ num1: Int, _ num2: Int) -> Int{
