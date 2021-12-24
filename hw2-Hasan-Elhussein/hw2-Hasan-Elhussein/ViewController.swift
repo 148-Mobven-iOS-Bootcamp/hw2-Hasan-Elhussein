dc38055813450e53faaf5787e6e48f2ccdbe3b3b
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2021-12-24 21:48:10 +0300

    Add initial project files

---
+++ hw2-Hasan-Elhussein/hw2-Hasan-Elhussein/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw2-Hasan-Elhussein
+//
+//  Created by Hasan Elhussein on 24.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
